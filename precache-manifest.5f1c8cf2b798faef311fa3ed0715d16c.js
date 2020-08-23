a45ceb9245f47ac5683d24a975ff213c208a3e25
Harshil Goel <HarshilGoel@example.com>
2020-08-23 16:28:34 +0530

    Updates

---
+++ precache-manifest.5f1c8cf2b798faef311fa3ed0715d16c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cb31a2b633849ef7bd6214641136348",
+    "url": "/news-app/index.html"
+  },
+  {
+    "revision": "584801fc5bac6eb4d526",
+    "url": "/news-app/static/css/main.e2162c11.chunk.css"
+  },
+  {
+    "revision": "5aeb53ad287d3ca5fce1",
+    "url": "/news-app/static/js/2.dafc56bd.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/news-app/static/js/2.dafc56bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "584801fc5bac6eb4d526",
+    "url": "/news-app/static/js/main.39486979.chunk.js"
+  },
+  {
+    "revision": "27f8ed6d9067f98a3af8",
+    "url": "/news-app/static/js/runtime-main.7d78981f.js"
+  }
+]);
